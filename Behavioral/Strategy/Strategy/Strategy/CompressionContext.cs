fa27e45d2eace6c94b14b81a5ce08d09b90033bf
pri.tom78 <paul15-5928@diu.edu.bd>
2022-06-17 21:15:19 +0600

    Strategy Design Pattern Implemented

---
+++ Behavioral/Strategy/Strategy/Strategy/CompressionContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class CompressionContext
+    {
+        private ICompression _compression;
+        public CompressionContext(ICompression compression)
+        {
+            _compression = compression;
+        }
+
+        public void SetStrategy(ICompression Compression)
+        {
+            _compression = Compression;
+        }
+        public void CreateArchive(string compressedArchiveFileName)
+        {
+            _compression.CompressFolder(compressedArchiveFileName);
+        }
+    }
+}
---
+++ Behavioral/Strategy/Strategy/Strategy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //zip compress
+            CompressionContext compressionContext = new CompressionContext(new ZipCompression());
+            compressionContext.CreateArchive("DotnetHelloWorld");
+
+            Console.WriteLine();
+
+            //rar compress
+            compressionContext.SetStrategy(new RarCompression());
+            compressionContext.CreateArchive("DotnetHelloWorld");
+
+            Console.ReadKey();
+        }
+    }
+}
