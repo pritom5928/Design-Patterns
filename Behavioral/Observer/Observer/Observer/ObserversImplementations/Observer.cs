23a678770d9baf1bee67c3462c8debc9a6bc8158
pri.tom78 <paul15-5928@diu.edu.bd>
2022-06-26 00:45:06 +0600

    Observer Design Pattern Implemented

---
+++ Behavioral/Observer/Observer/Observer/ObserversImplementations/Observer.cs
@@ -0,0 +1,25 @@
+﻿using Observer.ObserversInterface;
+using Observer.SubjectInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer.ObserversImplementations
+{
+    public class Observer : IObserver
+    {
+        public string UserName { get; set; }
+        public Observer(string userName, ISubject subject)
+        {
+            UserName = userName;
+            subject.RegisterObserver(this);
+        }
+
+        public void update(string availabiliy)
+        {
+            Console.WriteLine("Hello " + UserName + ", Product is now " + availabiliy + " on Amazon");
+        }
+    }
+}
---
+++ Behavioral/Observer/Observer/Observer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Observer.SubjectImplementations;
+using System;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject RedMi = new Subject("Redmi Note 7 Pro", 21000, "Out of Stock");
+
+            //User Pritom will be created and user1 object will be registered to the subject
+            Observer.ObserversImplementations.Observer user1 = new Observer.ObserversImplementations.Observer("Pritom", RedMi);
+
+            //User Jugal will be created and user1 object will be registered to the subject
+            Observer.ObserversImplementations.Observer user2 = new Observer.ObserversImplementations.Observer("Jugal", RedMi);
+
+            //User Pinke will be created and user3 object will be registered to the subject
+            Observer.ObserversImplementations.Observer user3 = new Observer.ObserversImplementations.Observer("Pinke", RedMi);
+
+
+            Console.WriteLine("Red Mi Mobile current state : " + RedMi.getAvailability());
+            Console.WriteLine();
+
+
+            // Now product is available
+            RedMi.setAvailability("Available");
+            Console.Read();
+        }
+    }
+}
